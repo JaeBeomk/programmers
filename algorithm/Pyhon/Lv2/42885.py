9ef13b24d5a92d63f95a8e7cde0370b5c53e17c8
JaeBeomk <kimjm3435@naver.com>
2024-03-15 17:52:07 +0900

    [author] : beomzh [message] : company

--- algorithm/Pyhon/Lv2/42885.py
+++ algorithm/Pyhon/Lv2/42885.py
@@ -1,34 +1,7 @@
 # 구명보트 LV2 Greedy알고리즘
 # 무인도에 갇힌 사람을 구명보트로 구출 - > 2명씩 무게제한도 있음
 # 무게제한을 고려해 최소의 보트 구하기
-# stack 구조로 구하자
-# 최소로 구해야 하므로 두개를 지우자
-# 틀려서 다시
-def solution(people, limit):
-    people=sorted(people)
-    cnt=0
-    boat=0
-    weigth=[]
-    weigth_r=[]
-    for i in people:
-        # print(i)
-        weigth.append(i)
-        boat+=1
-        # print(weigth)
-        if boat==2 and sum(weigth)<=100:
-            # print(boat,"bbbbbbbbbbb")
-            cnt+=1
-            boat=0
-            weigth=[]
-        if sum(weigth)>limit:
-            weigth_r.append(weigth.pop())
-            # print(weigth_r,"rrrrrrrrr")
-    if len(weigth_r)>0 or len(weigth)>0:
-        # print(len(weigth_r))
-        # print(len(weigth))
-        cnt+=len(weigth_r)+len(weigth)
-        
-    return cnt
+
 def solution(people, limit):
     answer = 0
     people.sort()
