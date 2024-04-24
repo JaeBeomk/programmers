a9559244a8645d1f1be5016567eddb424ef4daae
jaebeomk <kimjm3435@naver.com>
2024-04-24 17:51:55 +0900

    [author] : beomzh [message] : commit msg

---
+++ Python/활용/대소구분.py
@@ -0,0 +1,11 @@
+# 대문자 소문자 치환 / 한번에 치환 
+mystring="aabdbdsASVGASv"
+print(mystring.upper())
+print(mystring.lower())
+print(mystring.swapcase())
+
+# 대소문자 확인은 
+print(mystring.isupper())
+print(mystring.islower())
+
+
---
+++ algorithm/Pyhon/Lv0/120893.py
@@ -0,0 +1,5 @@
+# 대문자는 소문자 소문자는 대문자로
+def solution(my_string):
+    return my_string.swapcase()
+
+print(solution("cccCCC"))
